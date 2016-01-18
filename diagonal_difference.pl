82acd5c6614f02889148c59ff49f26614b42d4cd
cstanwood <cstanwood@central1.com>
2016-01-18 15:57:02 -0800

    Fix diagonal so it doesn't use Strptime

--- diagonal_difference.pl
+++ diagonal_difference.pl
@@ -2,22 +2,22 @@
 use strict;
 use 5.018;
 
-chomp (my $size = <STDIN>);
-my $i = 0;
+chomp( my $size = <STDIN> );
+my $i     = 0;
 my @array = ();
-while($i < $size){
-   chomp(my $a_temp = <STDIN>);
-   my @a_t = split / /,$a_temp;
-   push @array,\@a_t;
-   $i++;
+while ( $i < $size ) {
+    chomp( my $a_temp = <STDIN> );
+    my @a_t = split / /, $a_temp;
+    push @array, \@a_t;
+    $i++;
 }
 
 my $sum1;
 my $sum2;
 
-for (my $i = 0; $i < $size; $i++) {
-   $sum1 += $array[$i][$i];   
-   $sum2 += $array[$i][$size - 1 - $i];   
+for ( my $i = 0 ; $i < $size ; $i++ ) {
+    $sum1 += $array[$i][$i];
+    $sum2 += $array[$i][ $size - 1 - $i ];
 }
 
-say abs($sum1 - $sum2);
+say abs( $sum1 - $sum2 );
