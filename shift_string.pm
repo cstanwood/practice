a42ad80c1871893f5e7651abb84ddac18f192344
cstanwood <cgstanwood@gmail.com>
2016-01-21 07:29:10 -0800

    More attempts to fix

--- shift_string.pm
+++ shift_string.pm
@@ -10,8 +10,9 @@ our @EXPORT_OK = qw(lexical_min);
 #my $string2 = "DANIEL";
 #my $string1 = "ABACABA";
 #my $string2 = "ABACABA";
-
-open(TESTS, "<", "input01.txt") or die "Cannot open input01.txt";
+#  AABABACABACABA
+  #AABBAACCAABBAA
+open( TESTS, "<", "input01.txt" ) or die "Cannot open input01.txt";
 
 chomp( my $num_tests = <TESTS> );
 my @tests;
@@ -23,9 +24,9 @@ for ( 1 .. $num_tests ) {
     push @results, $result;
 }
 
-open(RESULTS, ">", "my_output.txt") or die "Cannot open my_output.txt";
+open( RESULTS, ">", "my_output.txt" ) or die "Cannot open my_output.txt";
 for my $result (@results) {
-    print RESULTS $result;
+    print RESULTS $result . "\n";
 }
 
 sub lexical_min {
@@ -46,7 +47,7 @@ sub lexical_min {
         my $c1 = shift @array1;
         my $c2 = shift @array2;
         while ( defined($c1) and defined($c2) ) {
-            if ( lc $c1 lt lc $c2 or lc $c1 eq lc $c2 ) {
+            if ( $c1 lt $c2 ) {
                 push @result, $c1;
                 $c1 = shift @array1;
                 if ( not defined $c1 ) {
@@ -55,7 +56,23 @@ sub lexical_min {
                     $result = join( "", @result ) . join( "", @array2 );
                 }
             }
-            elsif ( lc $c2 lt lc $c1 ) {
+            #elsif ( $c1 eq $c2 ) {
+                #push @result, $c1;
+                #push @result, $c2;
+                ##$c1 = shift @array1;
+            #    $c2 = shift @array2;
+            #    if ( not defined $c1 ) {
+            #        push @result, $c2;
+            #        $c2 = undef;
+            #        $result = join( "", @result ) . join( "", @array2 );
+            #    }
+            #    elsif ( not defined $c2 ) {
+            #        push @result, $c1;
+            #        $c1 = undef;
+            #        $result = join( "", @result ) . join( "", @array1 );
+            #    }
+            #}
+            elsif ( $c1 eq $c2 or $c1 gt $c2 ) {
                 push @result, $c2;
                 $c2 = shift @array2;
                 if ( not defined $c2 ) {
